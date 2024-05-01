86efd57f8006839fb750fd23d8aa1d0403f025f5
Derek Willis <dwillis@gmail.com>
2024-05-01 11:21:56 -0400

    fixed file paths

--- dol_ofccp.sh
+++ dol_ofccp.sh
@@ -6,6 +6,6 @@ in2csv OFCCP-FOIA-Log-1-1-2015-7-6-2023.xlsx > foia_log1.csv
 in2csv FOIA-Log-7.1.23-12.31.23-508c.xlsx > foia_log2.csv
 sed 's/ *, */,/g;s/ *$//' foia_log2.csv > foia_log2_fixed.csv
 csvstack foia_log1.csv foia_log2_fixed.csv > combined_foia_log.csv
-csvcut -x data/combined_foia_log.csv > data/combined_foia_log_fixed.csv
+csvcut -x combined_foia_log.csv > combined_foia_log_fixed.csv
 cd ..
 python convert.py
